dd7859342578b2f5011cbd77889b2b536d892ed4
unknown <authorityuba@gmail.com>
2024-04-01 07:12:13 +0300

    Simple java project

---
+++ SimpleJavaProj.java
@@ -0,0 +1,10 @@
+public class SimpleJavaProj{
+
+	public static void main(String[] args) {
+
+        
+		System.out.println("This is a jenkins job to pull and build a simple java project!");
+	}
+	
+	
+}
