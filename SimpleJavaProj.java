3faf3101f5ddaa0fd370f9911cd4098f90928832
unknown <authorityuba@gmail.com>
2024-04-01 07:22:25 +0300

    simple java project 2

--- SimpleJavaProj.java
+++ SimpleJavaProj.java
@@ -3,7 +3,7 @@ public class SimpleJavaProj{
 	public static void main(String[] args) {
 
         
-		System.out.println("This is a jenkins job to pull and build a simple java project!");
+		System.out.println("This is a jenkins job to pull and build a simple java project from git-hub!");
 	}
 	
 	
